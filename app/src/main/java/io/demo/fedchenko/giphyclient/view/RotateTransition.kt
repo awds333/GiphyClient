5e3a73e4d218fa620613dfdd3603bc33fd4fc88e
FedchenkoYA <wn8yura@gmail.com>
2019-11-29 13:58:23 +0300

    Removed transition name selection in RotateTransition

--- app/src/main/java/io/demo/fedchenko/giphyclient/view/RotateTransition.kt
+++ app/src/main/java/io/demo/fedchenko/giphyclient/view/RotateTransition.kt
@@ -11,24 +11,20 @@ import android.view.ViewGroup
 class RotateTransition(context: Context, attributeSet: AttributeSet) :
     Transition(context, attributeSet) {
 
+    private var viewSelected = false
+
     override fun captureStartValues(transitionValues: TransitionValues?) {
         val view = transitionValues?.view ?: return
-        if (!view.transitionName.isNullOrEmpty()) {
-            val position = IntArray(2)
-            view.getLocationOnScreen(position)
-            transitionValues.values["startPosition"] = position[0] + (view.width / 2)
-            transitionValues.values["transitionName"] = view.transitionName
-        }
+        val position = IntArray(2)
+        view.getLocationOnScreen(position)
+        transitionValues.values["startPosition"] = position[0] + (view.width / 2)
     }
 
     override fun captureEndValues(transitionValues: TransitionValues?) {
         val view = transitionValues?.view ?: return
-        if (!view.transitionName.isNullOrEmpty()) {
-            val position = IntArray(2)
-            view.getLocationOnScreen(position)
-            transitionValues.values["endPosition"] = position[0] + (view.width / 2)
-            transitionValues.values["transitionName"] = view.transitionName
-        }
+        val position = IntArray(2)
+        view.getLocationOnScreen(position)
+        transitionValues.values["endPosition"] = position[0] + (view.width / 2)
     }
 
     override fun createAnimator(
@@ -36,8 +32,9 @@ class RotateTransition(context: Context, attributeSet: AttributeSet) :
         startValues: TransitionValues?,
         endValues: TransitionValues?
     ): Animator {
-        if (startValues?.values?.get("transitionName") == null)
+        if (viewSelected || startValues == null)
             return ValueAnimator.ofFloat(0f, 1f)
+        viewSelected = true
         val animator =
             if ((startValues.values?.get("startPosition") as Int) < (endValues?.values?.get("endPosition") as Int))
                 ValueAnimator.ofFloat(0f, 360f)
